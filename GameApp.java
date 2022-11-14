2f185fe91b309b46b28768b16fd8ad7684251647
Clemens <w1600062@apps.losrios.edu>
2022-11-14 13:40:35 -0800

    Displays the helicopter's current fuel and manually begins. The cloud shrinks as time continues. Included an image but hasn't been set as the game's background.

--- GameApp.java
+++ GameApp.java
@@ -7,6 +7,7 @@ import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -25,6 +26,8 @@ import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 interface Updatable{
@@ -41,6 +44,7 @@ abstract class GameObject extends Group {
     Translate translate;
     Scale scale;
     Rotate rotate;
+    private int finalSeed;
 
     public GameObject() {
         translate = new Translate();
@@ -62,15 +66,41 @@ abstract class GameObject extends Group {
         translate.setY(transY);
     }
     void add(Node node) {this.getChildren().add(node);}
+    void setSeed(int seed) {finalSeed+=seed;}
+    int getSeed() {return finalSeed;}
 
 }
-class Pond extends Circle {
+class Pond extends GameObject {
+    Circle aPond = new Circle();
+    Label percentage = new Label();
     public Pond( double sizeX, double sizeY) {
-        setRadius(Math.random()*25*Math.PI);
-        setCenterX(rand()*sizeX/2);
-        setCenterY(rand()*sizeY/2);
-        setStroke(Color.BLUE);
-        setFill(Color.BLUE);
+        aPond.setRadius(Math.random()*25*Math.PI);
+        aPond.setCenterX(rand()*sizeX/2);
+        aPond.setCenterY(rand()*sizeY/2);
+        aPond.setStroke(Color.BLUE);
+        aPond.setFill(Color.BLUE);
+
+        AnimationTimer loop = new AnimationTimer() {
+            double old = -1;
+            double elapsedTime = 0;
+            double pondP = 0;
+            @Override
+            public void handle(long nano) {
+                if (old < 0) old = nano;
+                double delta = (nano - old) / 1e9;
+                old = nano;
+                elapsedTime += delta;
+
+                percentage.setTranslateX(aPond.getCenterX());
+                percentage.setTranslateY(aPond.getCenterY());
+                percentage.setTextFill(Color.WHITE);
+                percentage.setText(String.format(
+                        "F: %.2f", pondP));
+            }
+        };
+        loop.start();
+        add(aPond);
+        add(percentage);
     }
     public void update() {
 
@@ -83,20 +113,48 @@ class Pond extends Circle {
         return toReturn;
     }
 }
-class Cloud extends Circle {
-    public Cloud(double sizeX, double sizeY) {
-        setRadius(Math.random()*25*Math.PI);
-        setCenterX(rand()*sizeX/2);
-        setCenterY(rand()*sizeY/2);
-        setStroke(Color.GRAY);
-        setFill(Color.GRAY);
+class Cloud extends GameObject {
+    Circle aCloud = new Circle();
+    Label percentage = new Label();
+    private int finalSeed;
+    public Cloud(double sizeX, double sizeY, int seed) {
+        aCloud.setRadius(Math.random()*25*Math.PI);
+        aCloud.setCenterX(rand()*sizeX/2);
+        aCloud.setCenterY(rand()*sizeY/2);
+        aCloud.setStroke(Color.GRAY);
+        aCloud.setFill(Color.GRAY);
+        seed = 28;
+
+        finalSeed = seed;
+        setSeed(finalSeed);
+        AnimationTimer loop = new AnimationTimer() {
+            double old = -1;
+            double elapsedTime = 0;
+            double cloudP = 0;
+            @Override
+            public void handle(long nano) {
+                if (old < 0) old = nano;
+                double delta = (nano - old) / 1e9;
+                old = nano;
+                elapsedTime += delta;
+
+                percentage.setTranslateX(aCloud.getCenterX());
+                percentage.setTranslateY(aCloud.getCenterY());
+                percentage.setTextFill(Color.WHITE);
+                percentage.setText(String.format(
+                        "F: %.2f", finalSeed - elapsedTime));
+                aCloud.setScaleX(aCloud.getScaleX() - 0.0005);
+                aCloud.setScaleY(aCloud.getScaleY() - 0.0005);
+            }
+        };
+        loop.start();
+        add(aCloud);
+        add(percentage);
     }
     public double rand() {
         double toReturn =  Math.random()
                 * (Math.random() - Math.random()) +
                 Math.random();
-        //System.out.println(toReturn);
-        //System.out.println(Math.random());
         return toReturn;
     }
 }
@@ -112,7 +170,8 @@ class Helipad extends Rectangle {
 class Helicopter extends GameObject {
     //Line xAxis;
     Ellipse myhelicopter;
-    Text text = new Text("100 %");
+    Rectangle head = new Rectangle();
+    Label text = new Label();
     public Helicopter() {
         //xAxis = new Line(-125,0,125,0);
         super();
@@ -121,14 +180,21 @@ class Helicopter extends GameObject {
         myhelicopter.setStroke(Color.MAGENTA);
         myhelicopter.setRadiusX(15);
         myhelicopter.setRadiusY(15);
+
         text.setFont(Font.font(20));
-        text.setFill(Color.WHITE);
+
+        head.setWidth(10);
+        head.setHeight(25);
+        head.setStroke(Color.MAGENTA);
+        head.setFill(Color.MAGENTA);
+
         add(myhelicopter);
         add(text);
+        add(head);
     }
 }
 class Background extends Pane {
-    Image image = new Image("file:Back.jpg");
+    Image image = new Image("file:brownBackground.png");
     ImageView iView = new ImageView(image);
     public void Background() {
         iView.setFitHeight(600);
@@ -144,15 +210,17 @@ class Game {
     Helipad helipad = new Helipad();
     Helicopter helicopter = new Helicopter();
     Pond pond = new Pond(size.getX(),size.getY());
-    Cloud clouds = new Cloud(size.getX(),size.getY());
+    Cloud clouds = new Cloud(size.getX(),size.getY(), 0);
 
     BooleanProperty left = new SimpleBooleanProperty();
     BooleanProperty right = new SimpleBooleanProperty();
     BooleanProperty up = new SimpleBooleanProperty();
     BooleanProperty down = new SimpleBooleanProperty();
+    BooleanProperty space = new SimpleBooleanProperty();
     public Game() {
         init(root);
-        root.setStyle("-fx-background-color: black;");
+        //root.setStyle("-fx-background-color: black;");
+        root.getChildren().add(background.iView);
 
         helipad.setX(size.getX()/2.5);
         helipad.setY(size.getY() - 120);
@@ -165,34 +233,54 @@ class Game {
                 helicopter, pond, clouds,
                 helicopter.myhelicopter,
                 helicopter.text);
-
  */
         helicopter.setLayoutX(size.getX()/2);
         helicopter.setLayoutY(size.getY()/2);
-        helicopter.text.setX(size.getX()/10 - 50);
-        helicopter.text.setY(size.getY()/10);
+        helicopter.head.setX(size.getX()/100 - 8);
+        helicopter.head.setY(size.getY()/100 - 39);
 
-        //helicopter.scale.setX(20);
+        //Group gCloud = new Group();
+        //List<Cloud> clouds = new LinkedList<>();
 
         AnimationTimer loop = new AnimationTimer() {
             double old = -1;
             double elapsedTime = 0;
             double speed = 2;
+            int cloudCount = 5;
             @Override
             public void handle(long nano) {
                 if (old < 0) old = nano;
                 double delta = (nano - old) / 1e9;
-
                 old = nano;
-                elapsedTime += delta;
-                if (left.get())
+                elapsedTime += delta*500;
+
+                helicopter.text.setTranslateX(36);
+                helicopter.text.setTextFill(Color.MAGENTA);
+                helicopter.text.setText(String.format(
+                        "F: %.2f", 25000 - elapsedTime));
+
+
+                if (left.get()) {
                     helicopter.setLayoutX(helicopter.getLayoutX() - speed);
-                else if (right.get())
+                    helicopter.setTranslateX(helicopter.getTranslateX() - speed);
+                }
+                else if (right.get()) {
                     helicopter.setLayoutX(helicopter.getLayoutX() + speed);
+                    helicopter.setTranslateX(helicopter.getTranslateX() + speed);
+                }
                 else if (up.get())
                     helicopter.setLayoutY(helicopter.getLayoutY() - speed);
                 else if (down.get())
                     helicopter.setLayoutY(helicopter.getLayoutY() + speed);
+                else if (space.get()){
+                    clouds.setSeed(1);
+                    System.out.println("Space");
+                }
+                /*
+                while (clouds.size() < cloudCount) {
+                    clouds.add(Cloud.make)
+                }
+                 */
             }
         };
         scene.setOnKeyPressed(e -> {
@@ -204,6 +292,8 @@ class Game {
                 up.set(true);
             if(e.getCode() == KeyCode.DOWN)
                 down.set(true);
+            if(e.getCode() == KeyCode.SPACE)
+                space.set(true);
         });
         scene.setOnKeyReleased(e -> {
             if(e.getCode() == KeyCode.LEFT)
@@ -214,6 +304,8 @@ class Game {
                 up.set(false);
             if(e.getCode() == KeyCode.DOWN)
                 down.set(false);
+            if(e.getCode() == KeyCode.SPACE)
+                space.set(false);
         });
         loop.start();
     }
